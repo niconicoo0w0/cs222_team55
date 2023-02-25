49a0d56b2ad0818c081c36d4718ccc4c6068ccae
Zhilan Wang <92745699+niconicoo0w0@users.noreply.github.com>
2023-02-24 18:18:47 -0600

    Add files via upload

---
+++ nodelogin/accountTableGenerate.sql
@@ -0,0 +1,15 @@
+ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'kwr2#1108UIUC';
+flush privileges;
+
+CREATE DATABASE IF NOT EXISTS `nodelogin` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `nodelogin`;
+
+CREATE TABLE IF NOT EXISTS `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (121, 'test121', 'test121', 'test@test.com');
