becffea5a3829e45086478d540f7cf948554010f
Kaiwen-Ren <92759144+Kaiwen-Ren@users.noreply.github.com>
2023-03-31 23:47:08 -0500

    Delete SearchBook.jsx

--- library_app/frontend/components/SearchBook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function SearchBooks() {
-    return <div className='container'>
-        <h1>This is search book Page</h1>
-    </div>
-}
-
-export default SearchBooks;
