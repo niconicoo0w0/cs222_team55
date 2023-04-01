f3f53c0d41d83d41c9958589f534f05889a70927
Kaiwen-Ren <92759144+Kaiwen-Ren@users.noreply.github.com>
2023-03-31 23:47:12 -0500

    Delete UpdateBook.jsx

--- library_app/frontend/components/UpdateBook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function UpdateBooks() {
-    return <div className='container'>
-        <h1>This is update book Page</h1>
-    </div>
-}
-
-export default UpdateBooks;
