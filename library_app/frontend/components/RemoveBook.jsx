a18d3b7357b57827d0cb51dcf08038dd06f7a228
Kaiwen-Ren <92759144+Kaiwen-Ren@users.noreply.github.com>
2023-03-31 23:47:02 -0500

    Delete RemoveBook.jsx

--- library_app/frontend/components/RemoveBook.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-function RemoveBooks() {
-    return <div className='container'>
-        <h1>This is remove books Page</h1>
-    </div>
-}
-
-export default RemoveBooks;
